7ff9c92119a4218e1389e49a82465ec0a48f7049
nishant jain <njain@cs.iitr.ac.in>
2021-03-27 05:56:55 +0530

    Updated the angle values for swap gate

--- Non_planar_graph/config.py
+++ Non_planar_graph/config.py
@@ -1,5 +1,3 @@
-import numpy as np
-import os
 
 tensorboard_log_path = "C:/Users/DELL/Desktop/Edinburgh_project/tensorboard_logs/"
 model_save_path = "C:/Users/DELL/Desktop/Edinburgh_project/models/"
@@ -8,5 +6,7 @@ lr = 0.001
 log_txt_path = "C:/Users/DELL/Desktop/Edinburgh_project/txt_logs"
 lr_decay_step = 5 
 lr_decay_rate = 0.001
-Num_Epochs = 2000
+Num_Epochs = 200
 Batch_Size = 1
+symbol_for_swap_phi = 0.7876796
+symbol_for_swap_theta = 1.9998921
